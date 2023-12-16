081802560e79c5af06c1c30ea0d39ecc29492db9
Superhero <wael22ka@hotmail.com>
2023-12-16 21:37:08 +0100

    add + update perfectly done

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "844ffa228dc5e597dcc8b45b9bdee9cb31b06dd2"
-app.config["MONGO_URI"] = "mongodb+srv://wael22ka:bdpcCdeVsPjQNdGX@cluster0.gaad0pv.mongodb.net/?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://wael22ka:ANs8rcM0OVhVezsY@cluster0.gaad0pv.mongodb.net/MyFirstDatabase?retryWrites=true&w=majority"
 
-#setuo PyMongo
-mongodb_client = PyMongo(app)
-db = mongodb_client.db
+try:
+    mongodb_client = PyMongo(app)
+    db = mongodb_client.db
+
+    if db is None:
+        print("Error: Could not connect to the database.")
+    else:
+        print("Connected to the database successfully.")
+except Exception as e:
+    print(f"Error: {e}")
 
 from application import routes
