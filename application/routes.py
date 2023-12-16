189404b74469a08338c8475283f5eb222c245140
Superhero <wael22ka@hotmail.com>
2023-12-16 18:35:58 +0100

    working on completing add Todo

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,11 @@
 from application import app
 from flask import render_template, redirect, request, flash
-
+from .forms import TodoForm
 
 @app.route("/")
 def index():
-    return render_template("layout.html")
+    return render_template("index.html", title="")
+@app.route("/add_todo")
+def add_todo():
+    form = TodoForm()
+    return render_template("add_todo.html", form = form)
