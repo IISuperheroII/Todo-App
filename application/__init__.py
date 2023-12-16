683d6f1996554cb56a02db4f6cabc326d34ef419
Superhero <wael22ka@hotmail.com>
2023-12-16 14:31:18 +0100

    a

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "844ffa228dc5e597dcc8b45b9bdee9cb31b06dd2"
-app.config["MONGO_URI"] = ""
+app.config["MONGO_URI"] = "mongodb+srv://wael22ka:bdpcCdeVsPjQNdGX@cluster0.gaad0pv.mongodb.net/?retryWrites=true&w=majority"
+
+#setuo PyMongo
+mongodb_client = PyMongo(app)
+db = mongodb_client.db
 
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -4,4 +4,4 @@ from flask import render_template, redirect, request, flash
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("layout.html")
